f75d720514a4cf24976fce10aadba93387624727
unknown <santi2357@hotmail.com>
2022-07-14 17:01:59 -0300

    homework done

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function factorear(num) {
@@ -6,7 +6,18 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let array = [1];
+  let aux = 2;
+  while (num != 1) {
+    if (num % aux === 0) {
+      array.push(aux);
+      num = num / aux;
+    } else {
+      aux++;
+    }
+  }
 
+  return array;
 }
 
 function bubbleSort(array) {
@@ -14,27 +25,22 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+    
 }
-
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
--- EjerciciosExtras/homework/Repaso-M1.js
+++ EjerciciosExtras/homework/Repaso-M1.js
@@ -16,7 +16,16 @@ const {
 
 var countArray = function(array) {
     // Tu código aca:
-    
+    let sum = 0;
+    if (array.length <= 0) return 0;
+    for (let i = 0; i < array.length; i++) {
+      if (!Array.isArray(array[i])) {
+        sum += array[i];
+      } else {
+        sum += countArray(array[i]);
+      }
+    }
+    return sum;
 }
 
 
@@ -39,7 +48,17 @@ var countArray = function(array) {
 
 var countProps = function(obj) {
     // Tu código aca:
-
+    let suma = 0;
+    for (const key in obj) {
+        suma++;
+        if(typeof obj[key] === 'object') {
+            if(!Array.isArray(obj[key])) {
+                suma+=countProps(obj[key])
+            }
+        }
+    }
+    
+    return suma;
 }
 
 
@@ -53,7 +72,18 @@ var countProps = function(obj) {
 
 LinkedList.prototype.changeNotNumbers = function(){
     // Tu código aca:
+    let current = this.head;
+    let count = 0;
 
+    while(current) {
+        if(isNaN(current.value)) {
+            current.value = 'Kiricocho';
+            count++;
+        }
+        current=current.next
+    }
+
+    return count;
 }
 
 
@@ -67,7 +97,18 @@ LinkedList.prototype.changeNotNumbers = function(){
 
 var mergeQueues = function(queueOne, queueTwo) {
     // Tu código aca:
+    let newQ = new Queue();
 
+    while(queueOne.size() || queueTwo.size()) {
+        let firstElement = queueOne.dequeue();
+        let secondElement = queueTwo.dequeue();
+        if(firstElement) newQ.enqueue(firstElement);
+        if(secondElement) newQ.enqueue(secondElement);
+    }
+
+
+
+    return newQ;
 }
 
 
@@ -82,14 +123,19 @@ var mergeQueues = function(queueOne, queueTwo) {
 
 var closureMult = function(multiplier) {
     // Tu código aca:
-
+    return function(num) {
+        return num * multiplier;
+    }
 }
 
 // Implementar el método sum dentro del prototype de BinarySearchTree
 // que debe retornar la suma total de los valores dentro de cada nodo del arbol
 BinarySearchTree.prototype.sum = function() {
     // Tu código aca:
-
+    if(!this.right && !this.left) return this.value
+    if(!this.right && this.left) return this.value + this.left.sum()
+    if(!this.left && this.right) return this.value + this.right.sum()
+    if(this.left && this.right) return this.value + this.left.sum() + this.right.sum()
 }
 
 module.exports = {
